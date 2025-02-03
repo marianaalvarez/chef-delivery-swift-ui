1a5880cffa935546bd252ad3286e5df3cae6e410
Mariana Alvarez de Carvalho <marialvarez@Marianas-MacBook-Pro.local>
2025-02-02 21:24:09 -0300

    Create a timer to change carousel automatically every 3 seconds.

--- ChefDelivery/CarouselView/CarouselTabView.swift
+++ ChefDelivery/CarouselView/CarouselTabView.swift
@@ -8,19 +8,29 @@
 import SwiftUI
 
 struct CarouselTabView: View {
+    @State private var currentIndex = 1
     let orderMocks: [OrderType] = [
         OrderType(id: 1, name: "barbecue-banner", image: "barbecue-banner"),
         OrderType(id: 2, name: "brazilian-meal-banner", image: "brazilian-meal-banner"),
         OrderType(id: 3, name: "pokes-banner", image: "pokes-banner")
     ]
     var body: some View {
-        TabView {
+        TabView(selection: $currentIndex) {
             ForEach(orderMocks) { mock in
                 CarouselItemView(orderType: mock)
+                    .tag(mock.id)
             }
         }
         .frame(height: 180)
         .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+                if currentIndex > orderMocks.count {
+                    currentIndex = 1
+                }
+                currentIndex += 1
+            }
+        }
     }
 }
 
